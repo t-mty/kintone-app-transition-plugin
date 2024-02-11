d8955961617bc8c00a5d0e86bcf0c53f6d474eeb
mty <73290752+t-mty@users.noreply.github.com>
2024-02-11 12:40:21 +0900

    Add files via upload

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -6,21 +6,58 @@ jQuery.noConflict();
   kintone.events.on('mobile.app.record.index.show', function() {
     var config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-    var spaceElement = kintone.mobile.app.getHeaderSpaceElement();
-    if (spaceElement === null) {
+    var $spaceElement = kintone.mobile.app.getHeaderSpaceElement();
+    if ($spaceElement === null) {
       throw new Error('The header element is unavailable on this page');
     }
-    var fragment = document.createDocumentFragment();
-    var headingEl = document.createElement('h3');
-    var messageEl = document.createElement('p');
 
-    messageEl.classList.add('plugin-space-message');
-    messageEl.textContent = config.message;
-    headingEl.classList.add('plugin-space-heading');
-    headingEl.textContent = 'Hello kintone plugin!';
+    //configの値を取得する
+    var $array = config.appButtons.split(',');
+    var $appButtonsArray = [];
+    while ($array.length) $appButtonsArray.push($array.splice(0, 2));
+    // console.log($appButtonsArray);
 
-    fragment.appendChild(headingEl);
-    fragment.appendChild(messageEl);
-    spaceElement.appendChild(fragment);
+    //ボタンを設置する
+    for (var i=0; i<$appButtonsArray.length; i++) {
+      var $span = document.createElement('span');
+      var $button = document.createElement('button');
+      $button.innerText = $appButtonsArray[i][0];
+      $button.className = 'custom-app-button';
+      var url = $appButtonsArray[i][1];
+      console.log(url);
+      $button.setAttribute('onclick', `location.href='${url}'`);
+      $span.append($button);
+      $spaceElement.append($span);
+    }
   });
+
 })(jQuery, kintone.$PLUGIN_ID);
+
+
+
+// jQuery.noConflict();
+
+// (function($, PLUGIN_ID) {
+//   'use strict';
+
+//   kintone.events.on('mobile.app.record.index.show', function() {
+//     var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
+//     var spaceElement = kintone.mobile.app.getHeaderSpaceElement();
+//     if (spaceElement === null) {
+//       throw new Error('The header element is unavailable on this page');
+//     }
+//     var fragment = document.createDocumentFragment();
+//     var headingEl = document.createElement('h3');
+//     var messageEl = document.createElement('p');
+
+//     messageEl.classList.add('plugin-space-message');
+//     messageEl.textContent = config.message;
+//     headingEl.classList.add('plugin-space-heading');
+//     headingEl.textContent = 'Hello kintone plugin!';
+
+//     fragment.appendChild(headingEl);
+//     fragment.appendChild(messageEl);
+//     spaceElement.appendChild(fragment);
+//   });
+// })(jQuery, kintone.$PLUGIN_ID);
